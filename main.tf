7ed3636f0e528267dbf837c8de3c5ce1f4fdfded
Pierrestap <pierreridderstap@gmail.com>
2022-09-04 10:21:50 +0200

    this time for real blank

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=2.5.0"
+      version = "3.21.1"
     }
   }
 }
@@ -14,22 +14,4 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 
   features {}
-}
-
-module "cluster" {
-  source                = "./modules/cluster/"
-  serviceprinciple_id   = var.serviceprinciple_id
-  serviceprinciple_key  = var.serviceprinciple_key
-  ssh_key               = var.ssh_key
-  location              = var.location
-  kubernetes_version    = var.kubernetes_version  
-  
-}
-
-module "k8s" {
-  source                = "./modules/k8s/"
-  host                  = "${module.cluster.host}"
-  client_certificate    = "${base64decode(module.cluster.client_certificate)}"
-  client_key            = "${base64decode(module.cluster.client_key)}"
-  cluster_ca_certificate= "${base64decode(module.cluster.cluster_ca_certificate)}"
 }
