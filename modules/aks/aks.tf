468cc93b4b8cef4fd6f00519630273d1c85c23c6
Pierrestap <pierreridderstap@gmail.com>
2022-09-04 20:29:58 +0200

    scaled to 2 nodes

--- modules/aks/aks.tf
+++ modules/aks/aks.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "prozyne-aks-dev" {
   
   default_node_pool {
     name       = "default"
-    node_count = 1
+    node_count = 2
     vm_size    = "Standard_B2s"
     type       = "VirtualMachineScaleSets"
     os_disk_size_gb = 50
