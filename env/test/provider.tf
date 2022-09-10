1439d76a6008d1af001be0dfc49180cf9786fff4
Pierrestap <pierreridderstap@gmail.com>
2022-09-10 13:48:09 +0200

    made some changes to pipeline

--- env/test/provider.tf
+++ env/test/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
     backend "azurerm" {
-      resource_group_name  = "prozyne-dev"
-      storage_account_name = "devtfstateacc"
-      container_name       = "devtfstate"
-      key                  = "test.terraform.tfstate"
+      resource_group_name  = "rg_${var.current_branch_name}_infra"
+      storage_account_name = "prozynetfstateacc"
+      container_name       = "tfstate"
+      key                  = "${var.current_branch_name}.terraform.tfstate"
     }
 }
 
